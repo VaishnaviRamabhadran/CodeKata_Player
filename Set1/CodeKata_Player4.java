be9b5c4095f295ea90250c377718bd3a71eda4fc
VaishnaviRamabhadran <vaishu2395@gmail.com>
2019-05-12 16:22:55 +0530

    CodeKata_Player4

---
+++ Set1/CodeKata_Player4.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class CodeKata_Player4{
+public static void main(String[] args)
+{
+String s;
+Scanner sc=new Scanner(System.in);
+s=sc.nextLine();
+System.out.print(s+".");
+}
+}
