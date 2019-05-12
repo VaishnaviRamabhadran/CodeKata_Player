aa317f1099200cd7d6a5ba80981a2ee8d5f15a43
VaishnaviRamabhadran <vaishu2395@gmail.com>
2019-05-12 16:53:35 +0530

    Update CodeKata_Player8.java

--- Set1/CodeKata_Player8.java
+++ Set1/CodeKata_Player8.java
@@ -10,7 +10,7 @@ String[] c=s.split(" ");
 s="";
 for(int i=0;i<c.length;i++)
 {
-c[i]=Character.toUpperCase(c[i].charAt(0))+c[i].substring(1);
+c[i]=Character.toUpperCase(c[i].charAt(0))+c[i].substring(1).toLowerCase();
 s+=c[i]+" ";
 }
 System.out.print(s);
