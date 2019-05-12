1b45c6436ab3784baf3df247055786d3db7ffe11
VaishnaviRamabhadran <vaishu2395@gmail.com>
2019-05-12 16:07:56 +0530

    Player1

---
+++ Set1/CodeKata_Player1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class CodeKata_Player1{
+public static void main(String[] args)
+{
+String s,op="";
+Scanner sc=new Scanner(System.in);
+s=sc.nextLine();
+StringBuilder str=new StringBuilder(s);
+System.out.println(str.reverse());
+/*for(int i=s.length()-1;i>=0;i--)
+op+=s.charAt(i);
+System.out.println(op);*/
+}
+}
